96d52524b59ecbfbdf9a53dad01bc2c9d53fd2cf
ParnikaS-9 <parnika9@gmail.com>
2026-04-14 23:59:02 +0530

    movie-service initial commit

---
+++ movie-service/src/main/java/com/booking/movie_service/entity/Movie.java
@@ -0,0 +1,26 @@
+
+package com.booking.movie_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long movieId;
+    private String movieName;
+    private String duration;
+}
